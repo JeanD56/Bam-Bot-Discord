90d309e2faf5a323d832def534c00e0e5feebab3
Jean <62540718+JeanD56@users.noreply.github.com>
2021-10-25 04:48:58 +0200

    Update config.js

--- src/util/config.js
+++ src/util/config.js
@@ -2,5 +2,14 @@ module.exports = {
     TOKEN: process.env.TOKEN,
     OWNERID: process.env.OWNERID,
     PREFIX: process.env.PREFIX,
-    MONGOOSTRING: process.env.MOONGOSTRING
+    MONGOOSTRING: process.env.MOONGOSTRING,
+    GitHubLink: "https://github.com/JeanD56/JoJo_Bot",
+    dashboard: {
+        clientID: "423887705064079360",
+        oauthSecret: "OBSqiRJKBGVQXfROBgLEHNidDvl5gD48",
+        callbackurl: "http://localhost:3030/callback",
+        secret: "jojo c'est la vie",
+        domaine: "localhost",
+        port: "3030",
+    }
 }
