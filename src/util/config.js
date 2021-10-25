ba3afaa421285fa3b7f581352dd42fff1b67ff9a
Jean <62540718+JeanD56@users.noreply.github.com>
2021-10-25 03:03:56 +0200

    Update config.js

--- src/util/config.js
+++ src/util/config.js
@@ -1,6 +1,8 @@
+const Config = require('../../config.json')
+
 module.exports = {
-    TOKEN: process.env.TOKEN,
-    OWNERID: process.env.OWNERID,
-    PREFIX: "*",
-    MONGOOSTRING: process.env.MOONGOSTRING
+    TOKEN: process.env.TOKEN || Config.TOKEN,
+    OWNERID: process.env.OWNERSID || Config.OwnerID,
+    PREFIX: process.env.PREFIX || Config.Prefix,
+    MONGOOSTRING: process.env.MOONGOSTRING || Config.MOONGOSTRING
 }
