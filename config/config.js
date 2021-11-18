901f8397f0c6049d3a5d618020ab00a8ffae3fe0
Jean <62540718+JeanD56@users.noreply.github.com>
2021-11-18 02:02:35 +0100

    Create config.js

---
+++ config/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    TOKEN: "",
+    OWNERID: [],
+    PREFIX: "*",
+    MONGOOSTRING: "",
+    GitHubLink: "https://github.com/JeanD56/JoJo_Bot",
+    dashboard: {
+        clientID: "",
+        oauthSecret: "",
+        callbackurl: "http://localhost:3030/callback",
+        secret: "",
+        domaine: "localhost",
+        port: "3030",
+    }
+}
